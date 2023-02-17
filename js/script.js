a15dfcad54de122f33f37bbfd04c69fdff2a9d2e
Moammad Farhad <mfarhadattari>
2023-02-17 17:48:02 +0600

    italic style function added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ document.getElementById('bold').addEventListener('click',function(){
 document.getElementById('un-bold').addEventListener('click',function(){
     textArea.classList.remove('font-bold') ;
 })
+
+
+document.getElementById('italic').addEventListener('click' , function(){
+    textArea.classList.add('italic');
+})
+
+document.getElementById('un-italic').addEventListener('click' , function(){
+    textArea.classList.remove('italic');
+})
